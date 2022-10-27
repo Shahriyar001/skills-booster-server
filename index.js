571d210626c3b8de96594ec420d29bd459edd34c
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-10-27 10:15:41 +0600

    added ignore file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
 const categories = require('./data/categorys.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) => {
     res.send('APi running');
@@ -11,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
